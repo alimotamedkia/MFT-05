f1a3b073f88dcd5939ac32ccaf89897886741301
alimotamedkia <info@andishehparsi.ir>
2024-12-05 20:42:31 +0330

    added lambda function

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,12 @@
 fun main() {
-    findMax()
-check()
+//    findMax()
+//    check()
+//    lambda1()
+//    lambda2()
+    checkString()
 }
 
-fun findMax (): Int?{
+fun findMax(): Int? {
 
     val a = readln().toInt()
     val b = readln().toInt()
@@ -17,8 +20,7 @@ fun findMax (): Int?{
     if (list.isEmpty()) {
         println("هیچ مقداری وجود ندارد")
         return null
-    }
-    else {
+    } else {
         println("بالاترین مقدار ${list.max()} است")
         return list.max()
     }
@@ -26,7 +28,25 @@ fun findMax (): Int?{
 
 }
 
-fun check(){
+fun check() {
     val a = "ali.motamedkia@gmail.com"
     println(a.endsWith("@gmail.com"))
+}
+
+fun lambda1() {
+    val lambda = { a: Int, b: Int -> println(a * b) }
+    lambda(4, 3)
+}
+
+fun lambda2() {
+    val lambda = { a: Int -> println(a % 2 == 0) }
+    lambda(4)
+    //OR
+    val lambda2: (Int) -> Unit = { println(it % 2 == 0) }
+    lambda2(4)
+}
+
+fun checkString() {
+    val check: (String) -> Unit ={ input->println(input==input.reversed())}
+    check(readln())
 }
