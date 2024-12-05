97f2ddde3c539cf6a970b45b8af9d8262e67f8e1
alimotamedkia <info@andishehparsi.ir>
2024-12-05 18:58:49 +0330

    First Commit

---
+++ src/Main.kt
@@ -0,0 +1,31 @@
+fun main() {
+//    findMax()
+check()
+}
+
+fun findMax (): Int?{
+
+    val a = readln().toInt()
+    val b = readln().toInt()
+    val c = readln().toInt()
+
+    val list = mutableListOf<Int>()
+    list.add(a)
+    list.add(b)
+    list.add(c)
+
+    if (list.isEmpty()) {
+        println("هیچ مقداری وجود ندارد")
+        return null
+    }
+    else {
+        println("بالاترین مقدار ${list.max()} است")
+        return list.max()
+    }
+
+}
+
+fun check(){
+    val a = "ali.motamedkia@gmail.com"
+    println(a.endsWith("@gmail.com"))
+}
